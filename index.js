0f69f7be37ef24ead519c9201b980473e33fe562
naima-tushin <naima39tushin@gmail.com>
2024-07-13 02:12:07 +0600

    Added cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5000', 'http://localhost:5173']
+    origin: ['http://localhost:5000', 'http://localhost:5173', 'https://dylan-estate-tushin.netlify.app/sellerHomePage', 'https://dylan-server-side-oe32pu0sa-naima-tushins-projects.vercel.app/']
 }));
 app.use(express.json());
 app.use(bodyParser.json());
